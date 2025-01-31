70a085f77e97b45b255b3c745be1cb3fefa32332
clemeeento <clement.furnon@gmail.com>
2025-02-01 00:09:37 +0100

    Test et ameliorations des methodes de détection

--- Étape 39 - Devoir 03/DetecteurContours.hpp
+++ Étape 39 - Devoir 03/DetecteurContours.hpp
@@ -6,7 +6,7 @@ class DetecteurContours
 {
 public:
     // Détection des contours avec Sobel
-    static cv::Mat DetecterSobel(const cv::Mat &image, int tailleNoyau = 3)
+    static cv::Mat DetecterSobel(const cv::Mat &image, int tailleNoyau = 3, int tailleNoyauGaussien = 5)
     {
         // Vérifier si l'image est valide
         if (image.empty())
@@ -15,10 +15,13 @@ public:
             return cv::Mat();
         }
 
+        // Appliquer un flou gaussien pour réduire le bruit
+        cv::GaussianBlur(image, image, cv::Size(tailleNoyauGaussien, tailleNoyauGaussien), 1.0);
+
         // Calcul des gradients de Sobel en X et Y
         cv::Mat gradientX, gradientY, gradientTotal;
-        cv::Sobel(image, gradientX, CV_16S, 1, 0, tailleNoyau);
-        cv::Sobel(image, gradientY, CV_16S, 0, 1, tailleNoyau);
+        cv::Sobel(image, gradientX, CV_32F, 1, 0, tailleNoyau);
+        cv::Sobel(image, gradientY, CV_32F, 0, 1, tailleNoyau);
 
         // Magnitude des gradients pour obtenir les contours
         cv::Mat magnitude;
@@ -29,13 +32,13 @@ public:
         magnitude.convertTo(contours, CV_8U);
 
         // Seuillage pour obtenir uniquement les contours
-        cv::threshold(contours, contours, 50, 255, cv::THRESH_BINARY);
+        cv::threshold(contours, contours, 50, 255, cv::THRESH_BINARY_INV);
 
         return contours;
     }
 
     // Détection des contours avec Laplace
-    static cv::Mat DetecterLaplace(const cv::Mat &image, int tailleNoyau = 3)
+    static cv::Mat DetecterLaplace(const cv::Mat &image, int tailleNoyauLaplace = 3, int tailleNoyauGaussien = 5)
     {
         // Vérifier si l'image est valide
         if (image.empty())
@@ -44,22 +47,25 @@ public:
             return cv::Mat();
         }
 
+        // Appliquer un flou gaussien pour réduire le bruit
+        cv::GaussianBlur(image, image, cv::Size(tailleNoyauGaussien, tailleNoyauGaussien), 1.0);
+
         // Appliquer le filtre Laplacien
         cv::Mat laplace;
-        cv::Laplacian(image, laplace, CV_16S, tailleNoyau);
+        cv::Laplacian(image, laplace, CV_16S, tailleNoyauLaplace);
 
         // Conversion en format affichable
         cv::Mat contours;
         laplace.convertTo(contours, CV_8U);
 
         // Seuillage pour obtenir uniquement les contours
-        cv::threshold(contours, contours, 50, 255, cv::THRESH_BINARY);
+        cv::threshold(contours, contours, 50, 255, cv::THRESH_BINARY_INV);
 
         return contours;
     }
 
     // Détection des contours avec Canny
-    static cv::Mat DetecterCanny(const cv::Mat &image, int seuilBas = 100, int seuilHaut = 200)
+    static cv::Mat DetecterCanny(const cv::Mat &image, int seuilBas = 100, int seuilHaut = 200, int tailleNoyauGaussien = 5)
     {
         // Vérifier si l'image est valide
         if (image.empty())
@@ -68,9 +74,15 @@ public:
             return cv::Mat();
         }
 
+        // Appliquer un flou gaussien pour réduire le bruit
+        cv::GaussianBlur(image, image, cv::Size(tailleNoyauGaussien, tailleNoyauGaussien), 1.0);
+
         // Appliquer le filtre de Canny
         cv::Mat contours;
         cv::Canny(image, contours, seuilBas, seuilHaut);
+
+        // Inverser les couleurs
+        contours = 255 - contours;
 
         return contours;
     }
--- Étape 39 - Devoir 03/Homework03.cpp
+++ Étape 39 - Devoir 03/Homework03.cpp
@@ -3,12 +3,15 @@
 #include <opencv2/opencv.hpp>
 
 #include "Fonctions.hpp"
-#include "EvaluateurContours.hpp"
+#include "DetecteurContours.hpp"
 
 int main()
 {
     std::vector<cv::Mat> tableauOriginales; // Tableau pour les images originales
     std::vector<cv::Mat> tableauContours; // Tableau pour le contour des images
+    std::vector<cv::Mat> tableauContoursSobel; // Tableau pour le contour des images avec Sobel
+    std::vector<cv::Mat> tableauContoursLaplace; // Tableau pour le contour des images avec Laplace
+    std::vector<cv::Mat> tableauContoursCanny; // Tableau pour le contour des images avec Canny
 
     // Créer un tableau pour les images originales
     CreerTableauImages(tableauOriginales, "Originales");
@@ -16,17 +19,36 @@ int main()
     //Créer un tableau pour les contours des images
     CreerTableauImages(tableauContours, "Contours");
 
-    // Afficher les images originales
-    for(size_t i = 0; i < tableauOriginales.size(); ++i)
+    // Contour des images avec Sobel
+    for (size_t i = 0; i < tableauOriginales.size(); i++)
     {
-        AfficherImage("Image originale", tableauOriginales[i]);
+        // Détection des contours avec Sobel
+        tableauContoursSobel.push_back(DetecteurContours::DetecterSobel(tableauOriginales[i]));
+        // Afficher les images
+        cv::imshow("Image originale", tableauOriginales[i]);
+        cv::imshow("Contours Sobel", tableauContoursSobel[i]);
         cv::waitKey(0);
     }
 
-    // Afficher les contours des images
-    for(size_t i = 0; i < tableauContours.size(); ++i)
+    // Contour des images avec Laplace
+    for (size_t i = 0; i < tableauOriginales.size(); i++)
     {
-        AfficherImage("Contours", tableauContours[i]);
+        // Détection des contours avec Laplace
+        tableauContoursLaplace.push_back(DetecteurContours::DetecterLaplace(tableauOriginales[i]));
+        // Afficher les images
+        cv::imshow("Image originale", tableauOriginales[i]);
+        cv::imshow("Contours Laplace", tableauContoursLaplace[i]);
+        cv::waitKey(0);
+    }
+
+    // Contour des images avec Canny
+    for (size_t i = 0; i < tableauOriginales.size(); i++)
+    {
+        // Détection des contours avec Canny
+        tableauContoursCanny.push_back(DetecteurContours::DetecterCanny(tableauOriginales[i]));
+        // Afficher les images
+        cv::imshow("Image originale", tableauOriginales[i]);
+        cv::imshow("Contours Canny", tableauContoursCanny[i]);
         cv::waitKey(0);
     }
 
